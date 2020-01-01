4f75db7b80439d32fe1377ae16a21162d3f51c0e
lamter <lamter.fu@gmail.com>
2020-01-01 12:21:28 +0800

    feat: 更新2020年的期货交易节假日

--- test.py
+++ test.py
@@ -2,9 +2,14 @@ import tradingtime as tt
 import arrow
 import datetime
 
-tt.load_futures_tradingtime(arrow.get('2011-01-01').date())
+tt.get_trading_status('SA')
 
-td = arrow.get('2018-11-01 09:03:00+08:00').datetime
-for i in range(1):
-    td += datetime.timedelta(days=1)
-    print(td, tt.is_tradingday(td))
+from tradingtime import future
+print(future.CZCE_n)
+
+# tt.load_futures_tradingtime(arrow.get('2011-01-01').date())
+#
+# td = arrow.get('2018-11-01 09:03:00+08:00').datetime
+# for i in range(1):
+#     td += datetime.timedelta(days=1)
+#     print(td, tt.is_tradingday(td))
--- tests/test_futures.py
+++ tests/test_futures.py
@@ -1,13 +1,14 @@
 # coding:utf-8
-
-def test_load_tradingtime_json(tt):
-    print(tt)
-
-
-def test_sc(tt):
-    """
-    测试上海能源交易所的品种
-    :param tt:
-    :return:
-    """
-    print(tt.get_trading_status('sc'))
+import tradingtime as tt
+tt.get_trading_status('SA')
+# def test_load_tradingtime_json(tt):
+#     print(tt)
+#
+#
+# def test_sc(tt):
+#     """
+#     测试上海能源交易所的品种
+#     :param tt:
+#     :return:
+#     """
+#     print(tt.get_trading_status('sc'))
